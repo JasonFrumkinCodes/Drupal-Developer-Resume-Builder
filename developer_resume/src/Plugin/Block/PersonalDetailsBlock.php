f9bdfbbbf0844be98f238a5583564193bf0ccef6
JasonFrumkinCodes <156046453+JasonFrumkinCodes@users.noreply.github.com>
2024-01-25 17:02:42 -0600

    Updates to .module and PersonalDetailsBlock plugin

--- developer_resume/src/Plugin/Block/PersonalDetailsBlock.php
+++ developer_resume/src/Plugin/Block/PersonalDetailsBlock.php
@@ -27,14 +27,23 @@ class PersonalDetailsBlock extends BlockBase {
     $applicant_city_state = $config->get('developer_resume.applicant_city_state');
     $applicant_linkedin_profile = $config->get('developer_resume.applicant_linkedin_profile');
     $applicant_github_url = $config->get('developer_resume.applicant_github_url');
+    
+    $applicant_linkedin_profile = str_replace("https://","",$applicant_linkedin_profile);
+    $applicant_linkedin_profile = str_replace("http://","",$applicant_linkedin_profile);
+    $applicant_linkedin_profile = str_replace("www.","",$applicant_linkedin_profile);
+
+    $applicant_github_url = str_replace("https://","",$applicant_github_url);
+    $applicant_github_url = str_replace("http://","",$applicant_github_url);
+    $applicant_github_url = str_replace("www.","",$applicant_github_url);
+
     $output .= "<div id='personal_details_box'>";
         $output .= "<div class='applicant_full_name'>".$applicant_full_name."</div>";
         $output .= "<div class='career_path_title'>".$career_path_title."</div>";
         $output .= "<div class='applicant_email_address'><i class='fa-regular fa-envelope'></i>".$applicant_email_address."</div>";
         $output .= "<div class='applicant_phone_number'><i class='fa-solid fa-mobile-screen-button'></i>".$applicant_phone_number."</div>";
         $output .= "<div class='fa-location-dot'><i class='fa-solid fa-location-dot'></i>".$applicant_city_state."</div>";
-        $output .= "<div class='applicant_linkedin_profile'><i class='fa-brands fa-linkedin'></i><div class='linkedin_profile'>".$applicant_linkedin_profile."</div></div>";
-        $output .= "<div class='applicant_github_url'><i class='fa-brands fa-github'></i>".$applicant_github_url."</div>";
+        $output .= "<div class='applicant_linkedin_profile'><i class='fa-brands fa-linkedin'></i><div class='linkedin_profile'><a href='https://www.".$applicant_linkedin_profile."' target='_blank'>".$applicant_linkedin_profile."</a></div></div>";
+        $output .= "<div class='applicant_github_url'><i class='fa-brands fa-github'></i><a href='https://www.".$applicant_github_url."' target='_blank'>".$applicant_github_url."</a></div>";
     $output .= "</div>";
   	return [
       '#type' => 'markup',
